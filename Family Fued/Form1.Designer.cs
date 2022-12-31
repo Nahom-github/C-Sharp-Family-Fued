9b4f7db4f2be8332bd4e7e70c13c72ef029c5d7a
Nahom_github <93624579+Nahom-github@users.noreply.github.com>
2022-12-31 18:32:33 -0500

    Add files via upload

---
+++ Family Fued/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Family_Fued
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.txtanswer = new System.Windows.Forms.TextBox();
+            this.btnanswer = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblscore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbwrong3 = new System.Windows.Forms.PictureBox();
+            this.pbwrong2 = new System.Windows.Forms.PictureBox();
+            this.pbwrong1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbwrong3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbwrong2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbwrong1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(279, 101);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(42, 44);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "1";
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbl5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.Location = new System.Drawing.Point(475, 101);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(42, 44);
+            this.lbl5.TabIndex = 1;
+            this.lbl5.Text = "5";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(279, 161);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(42, 44);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "2";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbl6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6.Location = new System.Drawing.Point(475, 161);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(42, 44);
+            this.lbl6.TabIndex = 3;
+            this.lbl6.Text = "6";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbl7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl7.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl7.Location = new System.Drawing.Point(475, 231);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(42, 44);
+            this.lbl7.TabIndex = 4;
+            this.lbl7.Text = "7";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(279, 231);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(42, 44);
+            this.lbl3.TabIndex = 5;
+            this.lbl3.Text = "3";
+            // 
+            // lbl8
+            // 
+            this.lbl8.AutoSize = true;
+            this.lbl8.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbl8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl8.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl8.Location = new System.Drawing.Point(475, 296);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(42, 44);
+            this.lbl8.TabIndex = 6;
+            this.lbl8.Text = "8";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.lbl4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(279, 296);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(42, 44);
+            this.lbl4.TabIndex = 7;
+            this.lbl4.Text = "4";
+            // 
+            // txtanswer
+            // 
+            this.txtanswer.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtanswer.Location = new System.Drawing.Point(302, 384);
+            this.txtanswer.Name = "txtanswer";
+            this.txtanswer.Size = new System.Drawing.Size(164, 20);
+            this.txtanswer.TabIndex = 8;
+            // 
+            // btnanswer
+            // 
+            this.btnanswer.Location = new System.Drawing.Point(517, 384);
+            this.btnanswer.Name = "btnanswer";
+            this.btnanswer.Size = new System.Drawing.Size(75, 23);
+            this.btnanswer.TabIndex = 9;
+            this.btnanswer.Text = "Submit";
+            this.btnanswer.UseVisualStyleBackColor = true;
+            this.btnanswer.Click += new System.EventHandler(this.btnanswer_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblscore.Location = new System.Drawing.Point(307, 39);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(75, 22);
+            this.lblscore.TabIndex = 10;
+            this.lblscore.Text = "Score: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 48);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Name a country that\r\nspeaks Spanish";
+            // 
+            // pbwrong3
+            // 
+            this.pbwrong3.BackgroundImage = global::Family_Fued.Properties.Resources.x;
+            this.pbwrong3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbwrong3.Location = new System.Drawing.Point(729, 22);
+            this.pbwrong3.Name = "pbwrong3";
+            this.pbwrong3.Size = new System.Drawing.Size(65, 66);
+            this.pbwrong3.TabIndex = 14;
+            this.pbwrong3.TabStop = false;
+            this.pbwrong3.Visible = false;
+            // 
+            // pbwrong2
+            // 
+            this.pbwrong2.BackgroundImage = global::Family_Fued.Properties.Resources.x;
+            this.pbwrong2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbwrong2.Location = new System.Drawing.Point(648, 22);
+            this.pbwrong2.Name = "pbwrong2";
+            this.pbwrong2.Size = new System.Drawing.Size(65, 66);
+            this.pbwrong2.TabIndex = 13;
+            this.pbwrong2.TabStop = false;
+            this.pbwrong2.Visible = false;
+            // 
+            // pbwrong1
+            // 
+            this.pbwrong1.BackgroundImage = global::Family_Fued.Properties.Resources.x;
+            this.pbwrong1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbwrong1.Location = new System.Drawing.Point(568, 22);
+            this.pbwrong1.Name = "pbwrong1";
+            this.pbwrong1.Size = new System.Drawing.Size(65, 66);
+            this.pbwrong1.TabIndex = 12;
+            this.pbwrong1.TabStop = false;
+            this.pbwrong1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 353);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 54);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "**Note: The first letter \r\nof the name should be \r\ncapital and the rest lowercase" +
+    "**";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 123);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltime.Location = new System.Drawing.Point(769, 316);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(66, 24);
+            this.lbltime.TabIndex = 17;
+            this.lbltime.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(683, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 22);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Timer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(873, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbwrong3);
+            this.Controls.Add(this.pbwrong2);
+            this.Controls.Add(this.pbwrong1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.btnanswer);
+            this.Controls.Add(this.txtanswer);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbwrong3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbwrong2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbwrong1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.TextBox txtanswer;
+        private System.Windows.Forms.Button btnanswer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbwrong1;
+        private System.Windows.Forms.PictureBox pbwrong2;
+        private System.Windows.Forms.PictureBox pbwrong3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Family Fued/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Family_Fued
+    
+{
+    public partial class Form1 : Form
+    {
+        int number = 0; // used for score
+        int guess = 0; // used for number of guesses
+        bool spain = false;
+        bool mexico = false;
+        bool USA = false;
+        bool argentina = false; //Code so that players dont guess the same answer twice
+        bool costa = false;
+        bool chile = false;
+        bool colombia = false;
+        bool Cuba = false;
+       
+        SoundPlayer right = new SoundPlayer(Properties.Resources.winsound);
+        SoundPlayer wrong = new SoundPlayer(Properties.Resources.failsound);
+        int time = 60;
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnanswer_Click(object sender, EventArgs e)
+        {
+           
+                if (txtanswer.Text == "Spain" && !spain) // if player inputs the right answer their score goes up by the designated score
+                {
+                    right.Play();
+                    lbl1.Text = "Spain";
+                    number = number + 80;
+                    lblscore.Text = "Score:" + number + "";
+                    spain = true;
+                }
+                else if (txtanswer.Text == "Mexico" && !mexico)
+                {
+                    right.Play();
+                    lbl2.Text = "Mexico";
+                    number = number + 70;
+                    lblscore.Text = "Score:" + number + "";
+                    mexico = true;
+                }
+                else if (txtanswer.Text == "USA" && !USA)
+                {
+                    right.Play();
+                    lbl3.Text = "USA";
+                    number = number + 60;
+                    lblscore.Text = "Score:" + number + "";
+                    USA = true;
+                }
+                else if (txtanswer.Text == "Cuba" && !Cuba)
+                {
+                    right.Play();
+                    lbl4.Text = "Cuba";
+                    number = number + 50;
+                    lblscore.Text = "Score:" + number + "";
+                    Cuba = true;
+                }
+                else if (txtanswer.Text == "Argentina" && !argentina)
+                {
+                    right.Play();
+                    lbl5.Text = "Argentina";
+                    number = number + 40;
+                    lblscore.Text = "Score:" + number + "";
+                    argentina = true;
+                }
+                else if (txtanswer.Text == "Costa Rica" && !costa)
+                {
+                    right.Play();
+                    lbl6.Text = "Costa Rica";
+                    number = number + 30;
+                    lblscore.Text = "Score:" + number + "";
+                    costa = true;
+                }
+                else if (txtanswer.Text == "Chile" && !chile)
+                {
+                    right.Play();
+                    lbl7.Text = "Chile";
+                    number = number + 20;
+                    lblscore.Text = "Score:" + number + "";
+                    chile = true;
+                }
+                else if (txtanswer.Text == "Colombia" && !colombia)
+                {
+                    right.Play();
+                    lbl8.Text = "Colombia";
+                    number = number + 10;
+                    lblscore.Text = "Score:" + number + "";
+                    colombia = true;
+                }
+
+
+
+                else if (txtanswer.Text != "Spain" && guess == 0) // if the player guesses the wrong answer, the int guess will go up and if it hits two game is over
+                {
+                    wrong.Play();
+                    pbwrong1.Visible = true;
+                    MessageBox.Show("You have guessed an incorrect answer. You have 2 lives left.");
+                    guess++;
+                }
+                else if (txtanswer.Text != "Spain" && guess == 1)
+                {
+                    wrong.Play();
+                    pbwrong2.Visible = true;
+                    MessageBox.Show("You have guessed an incorrect answer. You have 1 lives left.");
+                    guess++;
+                }
+                else if (txtanswer.Text != "Spain" && guess == 2)
+                {
+                    wrong.Play();
+                    pbwrong3.Visible = true;
+                    MessageBox.Show("You have guessed an incorrect answer. You have 0 lives left. Game over.");
+                    Application.Exit();
+                }
+                if (number == 360) // if the player gets all the correct answers they will win the game
+                {
+                    MessageBox.Show("Congragulations, you have beaten the game");
+                    Application.Exit();
+                }
+            }
+
+            private void timer1_Tick(object sender, EventArgs e) // this is the timer for how long the player has to win the game
+            {
+                lbltime.Text = time.ToString();
+                time--;
+            
+                if (time == -1)
+                {
+                    timer1.Stop();
+                MessageBox.Show("You ran out of time. game over");
+                Application.Exit();
+                }
+            }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+                timer1.Start();
+            
+        }
+    }
+}
